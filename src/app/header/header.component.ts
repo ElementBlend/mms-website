90ff3cc5be21af7fffb9684c89d11358384e556c
SamLam140330 <lamlam0328@gmail.com>
2023-11-21 06:10:31 +0800

    Trying to deal with the header, currently converting some code from PHP to Typescript and Angular (Not complete)

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  constructor() { }
+  isActive = false;
+
+  toggleNavbar() {
+    this.isActive = !this.isActive;
+  }
+}
