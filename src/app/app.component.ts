70e2aae5ef4a0d3f9f737751304ecb034408c4ed
SamLam140330 <lamlam0328@gmail.com>
2023-11-22 20:12:27 +0800

    Improve routing and Not-found-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home'
+  },
+  {
+    path: '404',
+    component: PageNotFoundComponent,
+    title: '404 Not Found'
+  },
+  {
+    path: '**',
+    redirectTo: '/404'
   }
 ];
