97fd7692557bc1c1d03ec92588d4639d39c3aa4b
SamLam140330 <lamlam0328@gmail.com>
2023-11-24 22:15:28 +0800

    Update login logic and api in beckend

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,33 +1,28 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavbarHeaderService } from '../navbar-header.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule, HttpClientModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
   isActive = false;
-  isAuthenticated: boolean = false;
-  username: string = '';
   navbarItemsList: any[] = [];
+  clientCN: string = '';
 
   constructor(private http: HttpClient, private navbarHeaderService: NavbarHeaderService) {
     this.navbarItemsList = this.navbarHeaderService.getNavbarItems();
   }
 
   ngOnInit(): void {
-    this.getUsername().subscribe(username => {
-      this.username = username ?? '';
-      if (this.username) {
-        this.isAuthenticated = true;
-      }
+    this.http.get<any>('/api/login').subscribe(data => {
+      this.clientCN = data.clientCN;
     });
   }
 
@@ -38,12 +33,5 @@ export class HeaderComponent implements OnInit {
   toggleTheme() {
     // TODO: Implement toggle dark mode or light mode
     throw new Error('Method not implemented.');
-  }
-
-  getUsername(): Observable<string> {
-    return this.http.get('/api/getUsername', { responseType: 'text', observe: 'response' })
-      .pipe(
-        map(response => response.headers.get('X-SSL-Client-CN') ?? '')
-      );
   }
 }
