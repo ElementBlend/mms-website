9283302d883d429c244878b1561a5228d1aa941f
SamLam140330 <lamlam0328@gmail.com>
2024-08-11 23:50:56 +0800

    Improve text message

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   backendDomain: "localhost",
   backendPort: 3000,
-  webApiKey: "vBf8J+4@ztpcUm3k"
+  webApiKey: "a073b2c3482257bdf16d1f5f0183f87a"
 };
